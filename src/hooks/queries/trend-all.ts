674e1a8adb4a9c22b31d909898878310f4a066d7
Pouria Nasiri <nasiryporya1385@gmail.com>
2026-01-01 00:41:52 +0330

    fix trends type

--- src/hooks/queries/trend-all.ts
+++ src/hooks/queries/trend-all.ts
@@ -1,15 +1,14 @@
 import { API_ROUTES } from '@/config/api_routes'
 import { fetcher } from '@/lib/api/fetcher'
 import { APIsResponse } from '@/types/api-response'
-import { MovieCardData } from '@/types/movie'
-import { TVCardData } from '@/types/tv'
+import { MediaBase } from '@/types/media'
 import { useQuery } from '@tanstack/react-query'
 
 export const useTrendAll = () => {
-  return useQuery<APIsResponse<MovieCardData | TVCardData>>({
+  return useQuery<APIsResponse<MediaBase>>({
     queryKey: ['trend', 'all'],
     queryFn: () =>
-      fetcher<APIsResponse<MovieCardData | TVCardData>>(
+      fetcher<APIsResponse<MediaBase>>(
         API_ROUTES.trending.all
       ),
     staleTime: 5 * 60 * 1000,
